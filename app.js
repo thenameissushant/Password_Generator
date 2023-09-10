4437c1826ce6c0248248099ab0583f95886476a0
Sush@nt <thenameissushant@gmail.com>
2023-09-10 23:23:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const passwordBox = document.getElementById("password");
+const length = 12; //This will generate the password of length 12.
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@!#$%^&*()_+=-}{|\:><?/";
+const allChars = upperCase + lowerCase + symbol +  number;
+
+//Function to generate the password.
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length>password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)]; 
+    }
+    passwordBox.value = password;
+
+}
+
+ // Function to copy the Password.
+ function copyPassword() {
+    passwordBox.select();
+    document.execCommand("copy");
+}
